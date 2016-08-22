cca74d41f4b5f67d053f8d1b120075dd05ca0a8c
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2016-08-22 22:20:07 +0200

    Moved apps out of nmutils and added a setuptools install script.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "NanoMAX Analysis Utilities",
+    version = "0.1a0",
+    packages = find_packages(),
+    install_requires = ['numpy', 'h5py']
+    )
