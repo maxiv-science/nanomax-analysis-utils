862cd66a037bc27230c4afa2c3f3f628316d03d5
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2016-10-19 10:34:06 +0200

    Now loads initial masks

--- apps/maskMaker.py
+++ apps/maskMaker.py
@@ -14,11 +14,11 @@ class Plotter():
     and adds masked areas as instructed by the GUI.
     """
 
-    def __init__(self, image, outputFile):
+    def __init__(self, image, outputFile, initialMask):
         self.fig, self.ax = plt.subplots(ncols=1)
         self.image = image
         self.outputFile = outputFile
-        self.mask = np.ones(image.shape)
+        self.mask = initialMask
         self.vertices = []
         self.update()
 
@@ -26,7 +26,7 @@ class Plotter():
         # show a masked version of the image
         self.ax.clear()
         self.ax.imshow(np.log10(self.image), interpolation='none', cmap='jet')
-        self.ax.imshow((1 - self.mask)*.6, cmap=nmutils.utils.alpha2black, vmin=0, vmax=1)
+        self.ax.imshow((1 - self.mask)*.6, cmap=nmutils.utils.alpha2black, vmin=0, vmax=1, interpolation='none')
         plt.draw()
 
     def add_point(self, point):
@@ -110,15 +110,24 @@ class GuiListener(object):
         self.buttonDown = False
 
 
-# parse arguments
-if len(sys.argv) < 4:
-    print "\nUsage: maskMaker.py <data file> <hdf5 path> <output file> \n"
+# mandatory arguments
+if len(sys.argv) < 3:
+    print "\nUsage: maskMaker.py <data file> <hdf5 path> \n     [<output file> <initial mask file> <initial mask hdf5 path>] \n"
     try:
         __IPYTHON__
         raise Exception("too few input arguments")
     except NameError:
         sys.exit(0)
-fileName, hdfPath, outputFile = sys.argv[1:]
+fileName, hdfPath = sys.argv[1:3]
+
+# optional arguments
+outputFile, initialFile, initialHdfPath = 'out.hdf5', None, 'mask'
+if len(sys.argv) >= 4:
+    outputFile = sys.argv[3]
+if len(sys.argv) >= 5:
+    initialFile = sys.argv[4]
+if len(sys.argv) >= 6:
+    initialHdfPath = sys.argv[5]
 
 # load data and average first dim until it's two dimensional
 with h5py.File(fileName, 'r') as hf:
@@ -127,7 +136,15 @@ with h5py.File(fileName, 'r') as hf:
     while len(data.shape) > 2:
         data = np.mean(data, axis=0)
 
+# optionally load initial mask
+if initialFile:
+    with h5py.File(initialFile, 'r') as hf:
+        initialMask = hf.get(initialHdfPath)
+        initialMask = np.array(initialMask)
+else:
+    initialMask = np.ones(data.shape)
+
 # start the gui
-plotter = Plotter(data, outputFile)
+plotter = Plotter(data, outputFile, initialMask)
 a = GuiListener(plotter)
 plt.show()
