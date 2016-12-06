377410c2440d41062462dde99b156da5f6689cb1
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2016-12-06 14:52:42 +0100

    Rearranged the propagator util

--- nmutils/utils/utils.py
+++ nmutils/utils/utils.py
@@ -147,57 +147,6 @@ def binPixels(image, n=2):
             new[i, j] = np.round(np.mean(image[i * n : (i + 1) * n, j * n : (j + 1) * n], axis=(0, 1)))
     return new
 
-def propagateNearfield(A, psize, distances, energy):     
-
-    """           
-
-    Propagates the complex wavefront in N-by-N ndarray to the plane(s)
-    specified as distances. The physical spacing of array elements is 
-    psize, and the beam energy is specified in keV. An array 
-    length(distances) x N x N is returned.
-
-    The underlying code is ptypy's Geo class, which is essentially
-    wrapped here.
-    """     
-
-    import ptypy
-
-    # check for square matrix
-    try:
-        assert len(A.shape) == 2
-        assert A.shape[0] == A.shape[1]
-    except AssertionError:
-        raise RuntimeError("Wavefront array must be N x N")
-
-    # passing a single distance is allowed too
-    try:
-        len(distances)
-    except TypeError:
-        distances = [distances]
-
-    # parameters for the propagator
-    geo_pars = ptypy.core.geometry.DEFAULT.copy(depth=10)
-    geo_pars.energy = energy
-    geo_pars.distance = distances[0]
-    geo_pars.resolution = psize
-    geo_pars.psize = psize
-    geo_pars.shape = A.shape[0]
-    geo_pars.propagation = 'nearfield'
-
-    # create the propagator
-    geo = ptypy.core.geometry.Geo(pars=geo_pars)
-
-    # prepare a 3D matrix for propagated wavefronts
-    N = len(distances)
-    result = np.zeros((N,) + A.shape, dtype=A.dtype)
-
-    # iterate over the distances and propagate to there
-    for i in range(N):
-        geo.p.distance = distances[i]
-        geo.update()
-        result[i, :, :] = geo.propagator.fw(A)
-
-    return result
     
 # constants
 from matplotlib.colors import LinearSegmentedColormap
