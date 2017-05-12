5a49e83b139733bca2f1b88aee3b99de0a5fe2e3
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2017-05-12 12:36:50 +0200

    Added index selection to XrfWidget as well, plus inherited the map widget from XrdWidget

--- apps/scanViewer/widgets/XrfWidget.py
+++ apps/scanViewer/widgets/XrfWidget.py
@@ -7,81 +7,7 @@ import PyQt4
 from silx.gui.icons import getQIcon
 import numpy as np
 
-class MapWidget(PlotWindow):
-    """
-    A re-implementation of Plot2D, with customized tools.
-    """
-
-    def __init__(self, parent=None):
-        # List of information to display at the bottom of the plot
-        posInfo = [
-            ('X', lambda x, y: x),
-            ('Y', lambda x, y: y),
-            ('Data', self._getActiveImageValue)]
-
-        super(MapWidget, self).__init__(parent=parent, backend=None,
-                                     resetzoom=True, autoScale=False,
-                                     logScale=False, grid=False,
-                                     curveStyle=False, colormap=True,
-                                     aspectRatio=True, yInverted=True,
-                                     copy=True, save=True, print_=False,
-                                     control=False, position=posInfo,
-                                     roi=False, mask=True)
-        if parent is None:
-            self.setWindowTitle('comMapWidget')
-
-        self.setGraphTitle('Scan map')
-        self.setGraphXLabel('motor x [um]')
-        self.setGraphYLabel('motor y [um]')
-        self.setKeepDataAspectRatio(True)
-        self.setYAxisInverted(True)
-
-        # add an interpolation toolbar
-        self.interpolToolbar = self.addToolBar('Interpolation')
-        self.interpolBox = PyQt4.QtGui.QSpinBox(
-            toolTip='Map oversampling relative to average step size')
-        self.interpolBox.setRange(1, 50)
-        self.interpolBox.setValue(5)
-        self.interpolMenu = PyQt4.QtGui.QComboBox(
-            toolTip='Type of interpolation between scan positions')
-        self.interpolMenu.insertItems(1, ['nearest', 'linear', 'cubic'])
-        self.interpolToolbar.addWidget(self.interpolMenu)
-        self.interpolToolbar.addWidget(PyQt4.QtGui.QLabel(' N:'))
-        self.interpolToolbar.addWidget(self.interpolBox)
-
-        # customize the mask tools for use as ROI selectors
-        # unfortunately, tooltip and icon reset each other, so only changing the icon.
-        self.maskToolsDockWidget.setWindowTitle('scan map ROI')
-        self.maskAction.setToolTip('Select a scan map region of interest')
-        self.maskAction.setIcon(getQIcon('image-select-box'))
-
-        # add a button to toggle positions
-        self.positionsAction = PyQt4.QtGui.QAction('positions', self, checkable=True)
-        self.toolBar().addAction(self.positionsAction)
-
-        # add a profile tool
-        self.profile = ProfileToolBar(plot=self)
-        self.addToolBar(self.profile)
-
-    def _getActiveImageValue(self, x, y):
-        """Get value of active image at position (x, y)
-
-        :param float x: X position in plot coordinates
-        :param float y: Y position in plot coordinates
-        :return: The value at that point or '-'
-        """
-        image = self.getActiveImage()
-        if image is not None:
-            data, params = image[0], image[4]
-            ox, oy = params['origin']
-            sx, sy = params['scale']
-            if (y - oy) >= 0 and (x - ox) >= 0:
-                # Test positive before cast otherwisr issue with int(-0.5) = 0
-                row = int((y - oy) / sy)
-                col = int((x - ox) / sx)
-                if (row < data.shape[0] and col < data.shape[1]):
-                    return data[row, col]
-        return '-'
+from XrdWidget import MapWidget
 
 class SpectrumWidget(PlotWindow):
     """
@@ -123,14 +49,21 @@ class XrfWidget(PyQt4.QtGui.QWidget):
         # connect the positions button
         self.map.positionsAction.triggered.connect(self.togglePositions)
 
+        # connect the clicker box
+        self.map.indexBox.valueChanged.connect(self.selectByIndex)
+
         # connect the mask widget to the update
         self.map.maskToolsDockWidget.widget()._mask.sigChanged.connect(self.updateSpectrum)
         self.spectrum.curvesROIDockWidget.sigROISignal.connect(self.updateMap)
+
+        # keep track of map selections by ROI or by index
+        self.selectionMode = 'roi' # 'roi' or 'ind'
 
     def setScan(self, scan):
         self.scan = scan
         if not scan:
             return
+        self.map.indexBox.setMaximum(scan.nPositions - 1)
         self.resetMap()
         self.resetSpectrum()
 
@@ -182,30 +115,35 @@ class XrfWidget(PyQt4.QtGui.QWidget):
     def updateSpectrum(self):
         try:
             self.window().statusOutput('Building XRF spectrum...')
-            # get and check the mask array
-            mask = self.map.maskToolsDockWidget.widget().getSelectionMask()
-            if mask.sum() == 0:
-                # the mask is empty, don't waste time with positions
-                print 'building fluorescence spectrum from all positions'
-                data = np.mean(self.scan.data['xrf'], axis=0)
-            else:
-                # recreate the interpolated grid from above, to find masked
-                # positions on the oversampled grid
-                dummy = np.zeros(self.scan.nPositions)
-                x, y, z = self.scan.interpolatedMap(dummy, self.map.interpolBox.value(), origin='ul')
-                maskedPoints = np.vstack((x[np.where(mask)], y[np.where(mask)])).T
-                pointSpacing2 = (x[0,1] - x[0,0])**2 + (y[0,0] - y[1,0])**2
-                # go through actual positions and find the masked ones
-                maskedPositions = []
-                for i in range(self.scan.nPositions):
-                    # the minimum distance of the current position to a selected grid point:
-                    dist2 = np.sum((maskedPoints - self.scan.positions[i])**2, axis=1).min()
-                    if dist2 < pointSpacing2:
-                        maskedPositions.append(i)
-                print 'building fluorescence spectrum from %d positions'%len(maskedPositions)
-                # get the average and replace the image with legend 'data'
-                data = np.mean(self.scan.data['xrf'][maskedPositions], axis=0)
-            self.spectrum.addCurve(range(len(data)), data, legend='data')
+            if self.selectionMode == 'ind':
+                index = self.map.indexBox.value()
+                data = self.scan.data['xrf'][index]
+            elif self.selectionMode == 'roi':
+                self.indexMarkerOn(False)
+                mask = self.map.maskToolsDockWidget.widget().getSelectionMask()
+                if mask.sum() == 0:
+                    # the mask is empty, don't waste time with positions
+                    print 'building fluorescence spectrum from all positions'
+                    data = np.mean(self.scan.data['xrf'], axis=0)
+                else:
+                    # recreate the interpolated grid from above, to find masked
+                    # positions on the oversampled grid
+                    dummy = np.zeros(self.scan.nPositions)
+                    x, y, z = self.scan.interpolatedMap(dummy, self.map.interpolBox.value(), origin='ul')
+                    maskedPoints = np.vstack((x[np.where(mask)], y[np.where(mask)])).T
+                    pointSpacing2 = (x[0,1] - x[0,0])**2 + (y[0,0] - y[1,0])**2
+                    # go through actual positions and find the masked ones
+                    maskedPositions = []
+                    for i in range(self.scan.nPositions):
+                        # the minimum distance of the current position to a selected grid point:
+                        dist2 = np.sum((maskedPoints - self.scan.positions[i])**2, axis=1).min()
+                        if dist2 < pointSpacing2:
+                            maskedPositions.append(i)
+                    print 'building fluorescence spectrum from %d positions'%len(maskedPositions)
+                    # get the average and replace the image with legend 'data'
+                    data = np.mean(self.scan.data['xrf'][maskedPositions], axis=0)
+            self.spectrum.addCurve(range(len(data)), data, legend='data',
+                resetzoom = False)
             self.window().statusOutput('')
         except:
             self.window().statusOutput('Failed to build XRF spectrum. See terminal output.')
@@ -221,3 +159,21 @@ class XrfWidget(PyQt4.QtGui.QWidget):
             self.map.addCurve([], [], label='scan positions')
         self.map.setGraphXLimits(*xlims)
         self.map.setGraphYLimits(*ylims)
+
+    def indexMarkerOn(self, on):
+        index = self.map.indexBox.value()
+        if on:
+            self.map.addCurve([self.scan.positions[index, 0]], 
+                [self.scan.positions[index, 1]], symbol='o', color='red', 
+                linestyle=' ', label='index marker', resetzoom=False,
+                replace=False)
+        else:
+            self.map.addCurve([], [], label='index marker', 
+                resetzoom=False, replace=False)
+
+    def selectByIndex(self):
+        self.selectionMode = 'ind'
+        self.indexMarkerOn(True)
+        # clearing the mask also invokes self.updateSpectrum():
+        self.map.maskToolsDockWidget.widget().resetSelectionMask()
+        self.selectionMode = 'roi'
