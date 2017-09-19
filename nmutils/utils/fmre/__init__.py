662d307f95da0fd744398ca8fcfcc964e07bd620
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2017-09-19 15:21:35 +0200

    Added docstring to the fmre module

--- nmutils/utils/fmre/__init__.py
+++ nmutils/utils/fmre/__init__.py
@@ -1,3 +1,11 @@
+"""
+Optimization algorithms to 're-sample' and 'deblur' acquired
+fluorescence maps given a measured point spread function (probe
+intensity)
+
+Written by Tiago Joao Cunha Ramos <tiagoj@dtu.dk>
+"""
+
 import solvers
 import projectors
 import data
