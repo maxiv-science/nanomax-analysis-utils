0f0ef22ee7acdd17aae35c045abb3be77c19d021
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2017-12-21 11:23:04 +0100

    Removed obsolete and broken scan types

--- nmutils/core/__init__.py
+++ nmutils/core/__init__.py
@@ -9,11 +9,7 @@ except ImportError:
     
 from Scan import *
 from nanomax_june2016 import *
-from als import *
-from i13 import *
 from ptypy import *
 from nanomax_week48 import *
-from id13 import *
 from nanomax_april2017 import *
 from nanomax_nov2017 import *
-
