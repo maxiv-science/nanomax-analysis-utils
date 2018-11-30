f802e5f5576ac39344f431a776310440c1c588a1
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2018-11-30 16:46:51 +0100

    New ptycho template script

--- apps/basic_reconstruction_script.py
+++ apps/basic_reconstruction_script.py
@@ -16,6 +16,7 @@ detector = 'pilatus' # or 'merlin'
 folder = '/data/staff/nanomax/commissioning_2018-2/20181119-test/raw/01_sstar/'
 scannr = 18
 distance = 4.012
+energy = 10.0
 ########################################################################
 
 
@@ -30,21 +31,18 @@ p.scans.scan00 = u.Param()
 p.scans.scan00.name = 'Full'
 p.scans.scan00.data = u.Param()
 p.scans.scan00.data.name = 'NanomaxStepscanNov2018'
-p.scans.scan00.data.dataPath = folder
-p.scans.scan00.data.pilatusPath = p.scans.scan00.data.dataPath
-p.scans.scan00.data.pilatusPattern = 'scan_%%04u_%s_0000.hdf5' % {'pilatus': 'pil100k', 'merlin': 'merlin'}[detector]
-p.scans.scan00.data.datafile = folder.strip('/').split('/')[-1] + '.h5'
-p.scans.scan00.data.hdfPath = 'entry_%%04u/measurement/%s/data' % {'pilatus': 'Pilatus', 'merlin': 'Merlin'}[detector]
+p.scans.scan00.data.path = folder
 p.scans.scan00.data.maskfile = {'merlin': '/data/visitors/nanomax/common/masks/mask_merlin_combined.h5',
-                            'pilatus': None}[detector]
-p.scans.scan00.data.scannr = scannr
+								'pilatus': None}[detector]
+p.scans.scan00.data.scanNumber = scannr
+
 p.scans.scan00.data.shape = 256
 p.scans.scan00.data.save = None
 p.scans.scan00.data.center = None # auto, you can also set (i, j) center here.
 p.scans.scan00.data.orientation = {'merlin': (False, True, False), 'pilatus': None}[detector]
 p.scans.scan00.data.distance = distance
 p.scans.scan00.data.psize = {'pilatus': 172e-6, 'merlin': 55e-6}[detector]
-p.scans.scan00.data.energy = 10.0
+p.scans.scan00.data.energy = energy
 p.scans.scan00.data.min_frames = 10
 p.scans.scan00.data.load_parallel = 'all'
 
@@ -62,9 +60,9 @@ p.engines.engine00.name = 'DM'
 p.engines.engine00.numiter = 100
 p.engines.engine00.numiter_contiguous = 10
 
-p.engines.engine01 = u.Param()
-p.engines.engine01.name = 'ML'
-p.engines.engine01.numiter = 20
+#p.engines.engine01 = u.Param()
+#p.engines.engine01.name = 'ML'
+#p.engines.engine01.numiter = 20
 
 if LooseVersion(ptypy.version) < LooseVersion('0.3.0'):
     raise Exception('Use ptypy 0.3.0 or better!')
