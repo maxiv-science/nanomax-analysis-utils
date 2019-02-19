5bc2f26fcb2cc5c77c012e87779968db5d7f718a
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-02-19 23:20:19 +0100

    Improved complex image plotting util

--- nmutils/utils/plot_utils.py
+++ nmutils/utils/plot_utils.py
@@ -1,13 +1,36 @@
 """ This file contains isolated helper functions and constants related to plotting. """
 
 from matplotlib.colors import LinearSegmentedColormap
+from colorsys import hsv_to_rgb
+import numpy as np
 
-def complex2image(z):
-    """ Wraps a ptypy util """
-    import ptypy
-    rgb = ptypy.utils.complex2rgb(z)
-    return rgb / rgb.max()
+def complex2image(z, vmin=None, vmax=None, argmin=-np.pi, argmax=np.pi):
+    """
+    Combined theft from ptypy and stack overflow
+    """
 
+    arg = np.angle(z)
+    h = (arg.clip(argmin, argmax) - argmin) / (argmax - argmin) # [0, 1]
+
+    s = np.ones(z.shape)
+
+    v = abs(z)
+    if vmin is None:
+        vmin = v.min()
+    if vmax is None:
+        vmax = v.max()
+    if vmin==vmax:
+        v = np.ones_like(v) * v.mean()
+        v = v.clip(0.0, 1.0)
+    else:
+        assert vmin < vmax
+        v = (v.clip(vmin, vmax)-vmin)/(vmax-vmin)
+
+    c = np.vectorize(hsv_to_rgb) (h,s,v) # tuple
+    c = np.array(c)      # (3, m, n)
+    c = c.swapaxes(0, 2) # (n, m, 3)
+    c = c.swapaxes(0, 1) # (m, n, 3)
+    return c
     
 # constants
 alpha2red = LinearSegmentedColormap('alpha2red', 
