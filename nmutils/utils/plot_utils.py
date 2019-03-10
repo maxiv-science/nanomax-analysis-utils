cf406e2a0c777720a46be1a5c74fba09856ae2e4
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-03-10 22:49:55 +0100

    More improvements to complex2image function

--- nmutils/utils/plot_utils.py
+++ nmutils/utils/plot_utils.py
@@ -1,32 +1,37 @@
 """ This file contains isolated helper functions and constants related to plotting. """
 
 from matplotlib.colors import LinearSegmentedColormap
-from colorsys import hsv_to_rgb
+from colorsys import hsv_to_rgb, hls_to_rgb
 import numpy as np
 
-def complex2image(z, vmin=None, vmax=None, argmin=-np.pi, argmax=np.pi):
+def complex2image(z, vmin=None, vmax=None, argmin=-np.pi, argmax=np.pi, offset=0.0, cmap='hsv'):
     """
-    Combined theft from ptypy and stack overflow
+    Combined theft from ptypy and stack overflow. The cmap kwarg can be 'hsv' or 'hls',
+    where the latter goes from white (at vmin, lightness=1.0) to full color
+    (at vmax, lightness=0.5). The offset argument rotates the hue over the [0,1]
+    interval.
     """
 
     arg = np.angle(z)
-    h = (arg.clip(argmin, argmax) - argmin) / (argmax - argmin) # [0, 1]
-
     s = np.ones(z.shape)
-
-    v = abs(z)
+    h = (arg.clip(argmin, argmax) - argmin) / (argmax - argmin) # [0, 1]
+    h += offset
+    
+    r = np.abs(z)
     if vmin is None:
-        vmin = v.min()
+        vmin = r.min()
     if vmax is None:
-        vmax = v.max()
-    if vmin==vmax:
-        v = np.ones_like(v) * v.mean()
-        v = v.clip(0.0, 1.0)
-    else:
-        assert vmin < vmax
-        v = (v.clip(vmin, vmax)-vmin)/(vmax-vmin)
+        vmax = r.max()
 
-    c = np.vectorize(hsv_to_rgb) (h,s,v) # tuple
+    if cmap == 'hsv':
+        v = (r.clip(vmin, vmax) - vmin) / (vmax - vmin)
+        c = np.vectorize(hsv_to_rgb)(h,s,v) # tuple
+    elif cmap == 'hls':
+        l = (r.clip(vmin, vmax) - vmin) / (vmax - vmin) # [0, 1]
+        l = 1 - l * .5 # [1, .5]
+        c = np.vectorize(hls_to_rgb)(h,l,s) # tuple
+    else:
+        raise AttributeError('Invalid cmap, has to be hsv or hls.')
     c = np.array(c)      # (3, m, n)
     c = c.swapaxes(0, 2) # (n, m, 3)
     c = c.swapaxes(0, 1) # (m, n, 3)
@@ -68,3 +73,4 @@ alpha2black = LinearSegmentedColormap('alpha2black',
          'alpha': ((0.0, 0.0, 0.0),
                    (1.0, 1.0, 1.0))
                    })
+
