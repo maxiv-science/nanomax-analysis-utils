maxiv-science/nanomax-analysis-utils

commit 1faa072777d1287d9d9bdfb05de8d310cf742729

Adapted to the silx 0.10.1 API, scanViewer now requires this version or newer

apps/scanViewer/main.py
@@ -20,9 +20,9 @@ except ImportError:
 from distutils.version import LooseVersion
 try:
     import silx
-    assert LooseVersion(silx.version) >= LooseVersion('0.8.0')
+    assert LooseVersion(silx.version) >= LooseVersion('0.10.1')
 except:
-    raise Exception('This application requires silx >= 0.8.0')
+    raise Exception('This application requires silx >= 0.10.1')
 
 from silx.gui import qt
 print 'silx %s using' % silx.version, qt.BINDING
@@ -218,7 +218,7 @@ class ScanViewer(qt.QMainWindow):
             except MemoryError:
                 print "Out of memory! Consider cropping or binning your images"
                 has_xrd = False
-            except:
+            except AttributeError:
                 print "no xrd data found"
                 has_xrd = False
 
@@ -227,7 +227,7 @@ class ScanViewer(qt.QMainWindow):
                 scan_.addData(dataType='xrf', name='xrf', **opts)
                 print "loaded xrf data: %d positions, %d channels"%(scan_.data['xrf'].shape)
                 has_xrf = True
-            except:
+            except AttributeError:
                 print "no xrf data found"
                 has_xrf = False
 

apps/scanViewer/widgets/XrfWidget.py
@@ -3,6 +3,7 @@ from silx.gui.plot.Profile import ProfileToolBar
 from silx.gui.icons import getQIcon
 from silx.gui import qt
 import numpy as np
+import time
 
 from XrdWidget import MapWidget
 
@@ -51,10 +52,13 @@ class XrfWidget(qt.QWidget):
 
         # connect the mask widget to the update
         self.map.getMaskToolsDockWidget().widget()._mask.sigChanged.connect(self.updateSpectrum)
-        self.spectrum.getCurvesRoiDockWidget().sigROISignal.connect(self.updateMap)
+        self.spectrum.getCurvesRoiWidget().sigROISignal.connect(self.updateMap)
 
         # keep track of map selections by ROI or by index
         self.selectionMode = 'roi' # 'roi' or 'ind'
+
+        # workaround to avoid multiple updates
+        self.last_map_update = 0.0
 
     def setScan(self, scan):
         self.scan = scan
@@ -80,6 +84,9 @@ class XrfWidget(qt.QWidget):
     def updateMap(self):
         if self.scan is None:
             return
+        elif time.time() - self.last_map_update < .05:
+            return
+
         try:
             self.window().statusOutput('Building XRF map...')
             # workaround to avoid the infinite loop which occurs when both
@@ -89,16 +96,16 @@ class XrfWidget(qt.QWidget):
             xlims = self.map.getGraphXLimits()
             ylims = self.map.getGraphYLimits()
             # get ROI information
-            try:
-                rois = self.spectrum.getCurvesRoiWidget().getRois()
-                roiName = self.spectrum.getCurvesRoiWidget().currentROI
-                lower = int(np.floor(rois[roiName]['from']))
-                upper = int(np.ceil(rois[roiName]['to']))
-                print "building fluorescence map from channels %d to %d"%(lower, upper)
-                average = np.mean(self.scan.data['xrf'][:, lower:upper], axis=1)
-            except:
+            roi = self.spectrum.getCurvesRoiWidget().currentRoi
+            if roi is None:
                 print "building fluorescence map from the whole spectrum"
                 average = np.mean(self.scan.data['xrf'], axis=1)
+            else:
+                lower = int(np.floor(roi.getFrom()))
+                upper = int(np.ceil(roi.getTo()))
+                print "building fluorescence map from channels %d to %d"%(lower, upper)
+                average = np.mean(self.scan.data['xrf'][:, lower:upper], axis=1)
+
             # interpolate and plot map
             method = self.map.interpolMenu.currentText()
             sampling = self.map.interpolBox.value()
@@ -109,6 +116,7 @@ class XrfWidget(qt.QWidget):
             self.map.setGraphXLimits(*xlims)
             self.map.setGraphYLimits(*ylims)
             self.window().statusOutput('')
+            self.last_map_update = time.time()
         except:
             self.window().statusOutput('Failed to build XRF map. See terminal output.')
             raise

commit 35cc0cd63793d841f2fa7f8f3f3ec8322cecf53f

Fixes bug introduced by 1faa0727 pending a real solution

apps/scanViewer/main.py
@@ -218,7 +218,7 @@ class ScanViewer(qt.QMainWindow):
             except MemoryError:
                 print "Out of memory! Consider cropping or binning your images"
                 has_xrd = False
-            except AttributeError:
+            except (AttributeError, IOError):
                 print "no xrd data found"
                 has_xrd = False
 
@@ -227,7 +227,7 @@ class ScanViewer(qt.QMainWindow):
                 scan_.addData(dataType='xrf', name='xrf', **opts)
                 print "loaded xrf data: %d positions, %d channels"%(scan_.data['xrf'].shape)
                 has_xrf = True
-            except AttributeError:
+            except (AttributeError, IOError):
                 print "no xrf data found"
                 has_xrf = False
 