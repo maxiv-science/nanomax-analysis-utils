eb478ec0fa6c3d294e5b90ab70e25d0e6c1639d3
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-08-22 13:47:07 +0200

    Only Python 3 from here on

--- nmutils/__init__.py
+++ nmutils/__init__.py
@@ -1,5 +1,11 @@
 class NoDataException(Exception):
 	pass
 
+import sys
+try:
+    assert sys.version[0] == '3'
+except:
+    raise Exception("This is a Python 3 library")
+
 from . import core
 from . import utils
