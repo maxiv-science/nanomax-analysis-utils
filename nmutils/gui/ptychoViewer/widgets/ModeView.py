92a9208b2be54eaada0322f315565e9a19792f55
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2021-03-30 16:30:00 +0200

    Added zeta fit to the probe mode view

--- nmutils/gui/ptychoViewer/widgets/ModeView.py
+++ nmutils/gui/ptychoViewer/widgets/ModeView.py
@@ -4,6 +4,14 @@ from silx.gui import qt
 from silx.gui.plot import Plot1D
 from silx.gui.plot.StackView import StackView
 from ptypy.utils import ortho
+from scipy.optimize import leastsq
+
+def model(zeta, beta):
+    n = np.arange(len(beta))
+    return beta[0] * ((1 - zeta) / (1 + zeta))**n
+
+def residuals(zeta, beta):
+    return beta - model(zeta, beta)
 
 class ModeView(qt.QWidget):
     def __init__(self, *args, **kwargs):
@@ -20,8 +28,11 @@ class ModeView(qt.QWidget):
     def set_data(self, probes, psize):
         ampl, nplist = ortho(probes)
         edges = list(range(len(ampl)))
-        self.hist.addHistogram(ampl, edges, legend='data', fill=True, color='k')
-        self.hist.setGraphYLimits(0, 1)
+        self.hist.addHistogram(ampl, edges, legend='data', fill=True, color='b')
         self.hist.setGraphXLabel('Mode #')
-        self.hist.setGraphYLabel('Relative power')
+        self.hist.setGraphYLabel('Relative power $\\beta$')
         self.stack.setStack(np.abs(np.array(nplist))**2)
+        zeta = leastsq(func=residuals, x0=.5, args=(ampl,))[0][0]
+        self.hist.addCurve(edges, model(zeta, ampl), color='r', linewidth=2)
+        self.hist.setGraphYLimits(0, 1)
+        self.hist.setGraphTitle('$\\zeta$=%.2f (fit)'%zeta)
