3690c1bbbb096d5f72d191c9d0724a8cd9ce2b71
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2021-04-07 12:43:01 +0200

    Version bump for easybuild

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "NanoMAX Analysis Utilities",
-    version = "0.4.1",
+    version = "0.4.3",
     packages = find_packages(),
     install_requires = ['numpy', 'h5py', 'silx>=0.11'],
     scripts = ['apps/scanViewer', 'apps/ptychoViewer', 'apps/limaLiveViewer', 'apps/fluxMonitor.py', 'apps/streamLiveViewer.py'],
