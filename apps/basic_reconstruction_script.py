5835584ebc2932e5757ad7a142981d9cc3630d3e
Maik Kahnt <maik.kahnt@maxiv.lu.se>
2022-06-07 14:36:12 +0200

    added an RF script that can deal with both xspress3 and x3mini

--- apps/basic_reconstruction_script.py
+++ apps/basic_reconstruction_script.py
@@ -5,84 +5,186 @@ This script is adapted for and requires ptypy 0.3. It is tested with
 MPI on the compute cluster.
 """
 
+import os
 import sys
-import numpy as np
+import time
 import ptypy
 from ptypy.core import Ptycho
 from ptypy import utils as u
 from distutils.version import LooseVersion
+from mpi4py import MPI
 
-## simplest possible input #############################################
-detector   = 'eiger' # or 'merlin' or 'pilatus'
-folder     = '../../raw/sample'
-distance_m = 3.71	# distance between the sample and the detector in meters
-defocus_um = 500	# distance between the focus and the sample plane in micro meters -> used for inital probe
-scannr     = int(sys.argv[1])
-########################################################################
+############################################################################
+# hard coded user input 
+############################################################################
 
+detector         = 'eiger1m' # or 'merlin' or 'pilatus'
+beamtime_basedir = '/data/staff/nanomax/commissioning_2022-1/20220525_hdf_formats/'
+sample           = 'sample'
+scannr           = 0
+distance_m       = 3.62    # distance between the sample and the detector in meters
+defocus_um       = 500     # distance between the focus and the sample plane in micro meters -> used for inital probe
+#energy_keV      = 6.5     # incident photon energy in keV ... now read from scan file
+cropping         = 256
+binning          = 1
+
+if len(sys.argv)>=2:
+    # scan number is given as first argument of this script
+    scannr = int(sys.argv[1]) 
+
+if len(sys.argv)>=3:
+    # sample name (and thus data directory) is given as the 2nd argument 
+    cropping = str(sys.argv[2]) 
+
+############################################################################
+# some preparations before the actual reconstruction 
+############################################################################
+
+# define the output directories
+out_dir         = beamtime_basedir + '/process/' + sample + '/scan_' + str(scannr).zfill(6) + '/ptycho_ptypy/'
+out_dir_data    = out_dir + 'data/'
+out_dir_dumps   = out_dir + 'dumps/'
+out_dir_scripts = out_dir + 'scripts/'
+out_dir_rec     = out_dir + 'rec/'
+
+# and what the files are supposed to be called
+path_data       = out_dir_data  + 'data_scan_' + str(scannr).zfill(6) + '.ptyd'                             # the file with the prepared data
+path_dumps      = out_dir_dumps + 'dump_scan_' + str(scannr).zfill(6)+'_%(engine)s_%(iterations)04d.ptyr'   # intermediate results
+path_rec        = out_dir_rec   + 'rec_scan_' + str(scannr).zfill(6)+'_%(engine)s_%(iterations)04d.ptyr'    # final reconstructions (of each engine)
+
+# stuff to only do once
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+if rank==0:
+
+    # create output directories if it does not already exists
+    os.makedirs(out_dir,         exist_ok=True)
+    os.makedirs(out_dir_data,    exist_ok=True)
+    os.makedirs(out_dir_dumps,   exist_ok=True)
+    os.makedirs(out_dir_scripts, exist_ok=True)
+    os.makedirs(out_dir_rec,     exist_ok=True)
+
+    # copy this file into this directory with a tag made from the time and date this script was run
+    os.system('cp ./' + str(__file__) + ' ' + out_dir_scripts + time.strftime("%Y-%m-%d_%H%M") + '_' + str(__file__))
+
+############################################################################
+# creating the parameter tree
+############################################################################
 
 # General parameters
 p = u.Param()
 p.verbose_level = 3
 p.run = 'scan%d' % scannr
+
+# where to put the reconstructions
 p.io = u.Param()
-p.io.rfile = 'recons/%(run)s_%(engine)s.ptyr'
-p.io.autoplot = u.Param()
-p.io.autoplot.active = False
+p.io.home = out_dir_rec                     # where to save the final reconstructions
+p.io.rfile = path_rec                       # how to name those files for the final reconstructions
+p.io.autosave = u.Param()
+p.io.autosave.rfile = path_dumps            # where to save the intermediate reconstructions and how to name them
 
 # Scan parameters
 p.scans = u.Param()
 p.scans.scan00 = u.Param()
 p.scans.scan00.name = 'Full'
 p.scans.scan00.coherence = u.Param()
-p.scans.scan00.coherence.num_probe_modes = 1		# Number of probe modes
+p.scans.scan00.coherence.num_probe_modes = 1        # number of probe modes
+p.scans.scan00.coherence.num_object_modes = 1       # number of object modes
 
 p.scans.scan00.data = u.Param()
 p.scans.scan00.data.name = 'NanomaxContrast'
-p.scans.scan00.data.path = folder
+p.scans.scan00.data.path = beamtime_basedir+'/raw/'+sample+'/'
 p.scans.scan00.data.detector = detector
 p.scans.scan00.data.maskfile = {'merlin': '/data/visitors/nanomax/common/masks/merlin/latest.h5',
-								'pilatus': None,
-                                'eiger': None,}[detector]
+                                'pilatus': None,
+                                'eiger': '/data/visitors/nanomax/common/masks/eiger/eiger_4M_blinking_pixels.h5', # legacy
+                                'eiger1m': None, # none yet
+                                'eiger4m': '/data/visitors/nanomax/common/masks/eiger/eiger_4M_blinking_pixels.h5'}[detector]
 p.scans.scan00.data.scanNumber = scannr
-p.scans.scan00.data.xMotor = 'sx'
-p.scans.scan00.data.yMotor = 'sy'
-p.scans.scan00.data.zDetectorAngle = +0.78 # [deg]  # +0.78 measured Sep 2020
-p.scans.scan00.data.shape = 256
-p.scans.scan00.data.save = None
-p.scans.scan00.data.center = None # auto, you can also set (i, j) center here.
-p.scans.scan00.data.orientation = {'merlin': (False, False, True), 'pilatus': None, 'eiger': None}[detector]
-p.scans.scan00.data.distance = distance_m
-p.scans.scan00.data.psize = {'pilatus': 172e-6, 'merlin': 55e-6, 'eiger': 75e-6}[detector]
-#p.scans.scan00.data.energy = energy_keV
-p.scans.scan00.data.I0 = None # can be like 'alba2/1'
+p.scans.scan00.data.xMotor = 'pseudo/x'
+p.scans.scan00.data.yMotor = 'pseudo/y'
+p.scans.scan00.data.zDetectorAngle = 0.0    # rotation of the detector around the beam axis in [deg]
+p.scans.scan00.data.xyAxisSkewOffset = -1.8
+p.scans.scan00.data.shape = cropping        # size of the window of the diffraction patterns to be used in pixel
+p.scans.scan00.data.save = 'append'
+p.scans.scan00.data.dfile = path_data       # once all data is collected, save it as .ptyd file
+p.scans.scan00.data.center = None #(824,367)      # center of the diffraction pattern (y,x) in pixel or None -> auto
+p.scans.scan00.data.cropOnLoad = True       # only load used part of detector frames -> save memory
+                                            # requires center to be set explicitly
+p.scans.scan00.data.xMotorFlipped = True
+p.scans.scan00.data.yMotorFlipped = False
+p.scans.scan00.data.orientation = {'merlin':  (False, False, True), 
+                                   'pilatus': (False, True, False), 
+                                   'eiger':   (False, True, False), # legacy
+                                   'eiger1m': (False, True, False),
+                                   'eiger4m': (False, True, False)}[detector]
+p.scans.scan00.data.distance = distance_m   # distance between sample and detector in [m] 
+p.scans.scan00.data.psize = {'pilatus': 172e-6, 
+                             'merlin':   55e-6, 
+                             'eiger':    75e-6,   # legacy
+                             'eiger1m':  75e-6,
+                             'eiger4m':  75e-6}[detector]
+p.scans.scan00.data.rebin = binning 
+#p.scans.scan00.data.energy = energy_keV    # incident photon energy in [keV], now read from file
+p.scans.scan00.data.I0 = None              # can be like 'alba2/1'
 p.scans.scan00.data.min_frames = 10
 p.scans.scan00.data.load_parallel = 'all'
 
-# Scan parameters: illumination
+# scan parameters: il500lumination
 p.scans.scan00.illumination = u.Param()
-p.scans.scan00.illumination.model = None
-p.scans.scan00.illumination.aperture = u.Param()
-p.scans.scan00.illumination.aperture.form = 'rect'
-p.scans.scan00.illumination.aperture.size = 100e-9			           # at the focus
-p.scans.scan00.illumination.propagation = u.Param()
-p.scans.scan00.illumination.propagation.parallel = -1.*defocus_um*1e-6 # somehow this has to be negative to the basez axis 
-p.scans.scan00.illumination.diversity = u.Param()
-p.scans.scan00.illumination.diversity.noise = (.5, 1.0)
-p.scans.scan00.illumination.diversity.power = .1
-															           # -> being downstream of the focus means negative distance
 
-# Reconstruction parameters
+p.scans.scan00.illumination.model = None                              # option 1: probe is initialized from a guess
+p.scans.scan00.illumination.aperture = u.Param()
+p.scans.scan00.illumination.aperture.form = 'circ'                    # initial probe is a rectangle (KB focus)
+p.scans.scan00.illumination.aperture.size = 60e-9                     # of this size in [m] the focus
+p.scans.scan00.illumination.propagation = u.Param()
+p.scans.scan00.illumination.propagation.parallel = 1.*defocus_um*1e-6 # propagate the inital guess -> gives phase curvature
+
+#p.scans.scan00.illumination.model = 'recon'                           # option 2: probe is initialized from a previous reconstruction
+#p.scans.scan00.illumination.recon = u.Param()
+#p.scans.scan00.illumination.recon.rfile = ...                         # absolute path to a .ptyr file containing the probe to be used as initial guess
+#p.scans.scan00.illumination.aperture = u.Param()
+#p.scans.scan00.illumination.aperture.form = 'rect'                    # this aperture is not optional
+#p.scans.scan00.illumination.aperture.size = 10e-8                     # either make it very large, or cut down the loaded probe
+
+
+############################################################################
+# 1st use the difference map algorithm
+############################################################################
+
+# general
 p.engines = u.Param()
 p.engines.engine00 = u.Param()
 p.engines.engine00.name = 'DM'
-p.engines.engine00.numiter = 100
-p.engines.engine00.numiter_contiguous = 10
+p.engines.engine00.numiter = 200                     # number of iterations
+p.engines.engine00.numiter_contiguous = 100          # save a dump file every x iterations
+p.engines.engine00.probe_support = 3                 # non-zero probe area as fraction of the probe frame
+#p.engines.engine00.probe_update_start = 50          # number of iterations before probe update starts
 
+############################################################################
+# 2nd use the maximum likelyhood algorithm
+############################################################################
+
+# general
 p.engines.engine01 = u.Param()
 p.engines.engine01.name = 'ML'
-p.engines.engine01.numiter = 100
-p.engines.engine01.numiter_contiguous = 10
+p.engines.engine01.numiter = 800                    # number of iterations
+p.engines.engine01.numiter_contiguous = 100         # save a dump file every x iterations
+
+# position refinement
+#p.engines.engine01.position_refinement = u.Param()
+#p.engines.engine01.position_refinement.start = 10                      # Number of iterations until position refinement starts
+#p.engines.engine01.position_refinement.stop = None                     # Number of iterations after which positon refinement stops, If None, stops after last iteration
+#p.engines.engine01.position_refinement.interval = 10                   # Frequency of position refinement
+#p.engines.engine01.position_refinement.nshifts = 4*64                  # Number of random shifts calculated in each position refinement step (has to be multiple of 4)
+#p.engines.engine01.position_refinement.amplitude =  0.5*7.75e-9        # Distance from original position per random shift [m]
+#p.engines.engine01.position_refinement.max_shift = 1000e-9             # Maximum distance from original position [m]
+#p.engines.engine01.position_refinement.metric = 'fourier'
+#p.engines.engine01.position_refinement.record = True                   # record movement of positions
+
+############################################################################
+# start the reconstruction
+############################################################################
 
 if LooseVersion(ptypy.version) < LooseVersion('0.3.0'):
     raise Exception('Use ptypy 0.3.0 or better!')
---
+++ apps/xrf_pyMCA_fit.py
@@ -0,0 +1,216 @@
+########## DO THE FOLLOWING ##########
+# 1. Copy this file to a sub folder in the process folder of the experiment,
+# example: /data/visitors/nanomax/[proposal_id]/[session_id]/process/xrf
+# 2. Scans: A list of entries to be fitted, or one scan number as command line argument
+# 3. Sample name: Name of the sample
+# 4. Configuration file for the fitting
+# 5. Select the output files you want to generate
+
+session_path = '/data/visitors/nanomax/proposalID/visit/'
+sample_name = 'sample'
+config_files = 'blub.cfg'
+detector = 'x3mini'          # 'x3mini' or 'xspress3'
+channels = [0]
+
+make_pymca_h5        = True
+make_elements_h5     = True
+make_elements_tif    = True
+make_spectrum_fit_h5 = True
+
+#######################################
+I0_scale_factor = 1E-9 # default
+#######################################
+
+##############################################################################
+# A class to read NanoMAX X-ray fluorescence data, generate files suitable to
+# be analysed in PyMCA, do automated fitings to generate elemental images, and
+# do a fit of the average spectrum in the image stack. All output files are
+# HDF5 files. Optionally TIF files can be generated with elemental maps.
+##############################################################################
+import h5py
+import sys
+import os
+import time
+import numpy
+from PyMca5.PyMcaPhysics.xrf.FastXRFLinearFit import FastXRFLinearFit
+from PyMca5.PyMcaPhysics.xrf.XRFBatchFitOutput import OutputBuffer
+from PyMca5.PyMcaPhysics.xrf.ClassMcaTheory import McaTheory
+from PyMca5.PyMcaIO import ConfigDict
+from silx.gui import qt
+
+class xrfBatch():
+
+    # init with the most important information that is needed over and over again
+    def __init__(self, session_path, sample_name, scan_nr, config_file,
+                 detector = 'xspress3', channel = 3):
+        self.session_path = session_path
+        self.sample_name  = sample_name
+        self.scan_nr      = scan_nr
+        self.config_file  = config_file
+        self.detector     = detector
+        self.channel      = channel
+        self.create_out_dirs()
+
+    # define where to save all the data
+    def create_out_dirs(self):
+        self.out_dir = self.session_path+'process/'+self.sample_name+'/scan_'+str(self.scan_nr).zfill(6)+'/xrf_pymca/'
+        self.out_dir_pymca_file = self.out_dir+'data/'
+        self.out_dir_elements   = self.out_dir+'elements/'
+        self.out_dir_spectrum   = self.out_dir+'spectrum/'	
+        os.makedirs(self.out_dir, exist_ok=True)
+        os.makedirs(self.out_dir_pymca_file, exist_ok=True)
+        os.makedirs(self.out_dir_elements, exist_ok=True)
+        os.makedirs(self.out_dir_spectrum, exist_ok=True)
+
+    # Reads a NanoMAX file
+    def readData(self, I0_scale_factor=1E-9):
+        input_file=os.path.join(self.session_path,'raw', self.sample_name, '%06u.h5'%self.scan_nr)
+        print('Reading raw data file: %s' % input_file)
+        with h5py.File(input_file, mode='r') as fp:
+            self.cmd = fp['entry/description'][0]
+            shape = int(self.cmd.split()[8])+1, int(self.cmd.split()[4])+1
+            self.dwell = float(self.cmd.split()[-2])
+            self.dt = fp['entry/measurement/dt'][:]
+            nlines = len(self.dt)
+            if  nlines < shape[0]:
+                print('Lines in file (%d) and lines in scan command (%d) mismatch. Scan was probably interrupted.'%(nlines, shape[0]))
+                shape = nlines, shape[1]
+            npixels = shape[0] * shape[1]
+            print('Image shape (%d,%d)'%shape)
+            self.I0 = fp['entry/measurement/alba2/1'][0:npixels].reshape(*shape, -1)
+            #self.xrf = fp['entry/measurement/xspress3/frames'][0:npixels, 3, 0:2600].reshape(*shape, -1) #legacy
+
+            self.xrf = fp[f'entry/measurement/'{self.detector}'/data'][0:npixels, self.channel, 0:2600].reshape(*shape, -1) #legacy
+            self.xrf_norm = ((self.xrf*I0_scale_factor)/(self.I0*self.dwell))
+            self.xrf_avg = self.xrf_norm.sum(axis=(0,1))/npixels
+            self.energy = fp['entry/snapshot/energy'][:]
+            self.x = fp['entry/measurement/pseudo/x'][0:npixels].reshape(*shape)
+            self.y = fp['entry/measurement/pseudo/y'][0:npixels].reshape(*shape)
+
+    # Writes a PyMCA readible h5 file
+    def writePymcafile(self):
+        pymca_file = os.path.join(f'{self.out_dir_pymca_file}_data_pymca_{str(scan_nr).zfill(6)}_{self.channel}.h5')
+        print("Writing pymca file: %s" % pymca_file)
+        with h5py.File(pymca_file, 'w') as ofp:
+            ofp.create_dataset('xrf', data=self.xrf, compression='gzip')
+            ofp.create_dataset('xrf_normalized', data=self.xrf_norm, compression='gzip')
+            ofp.create_dataset('xrf_normalized_avg', data=self.xrf_avg, compression='gzip')
+            ofp['I0'] = self.I0[:, :, 0]
+            ofp['I0_average'] = numpy.mean(self.I0)
+            ofp['dwell_time'] = self.dwell
+            ofp['energy'] = self.energy
+            ofp['positions'] = [self.x,self.y]
+            ofp['detector'] = self.detector
+            ofp['channel'] = self.channel
+
+    # Linear fitting of xrf stack data. Generate a h5 and/or tiff files with element concentration maps
+    def fitElementsToFile(self, make_elements_h5=True, make_elements_tif=False):
+        elem_file_name = f'fitted_elements_{str(scan_nr).zfill(6)}_{self.channel}'
+        print('Generating elemental maps file: %s.h5' % elem_file_name)
+        fastFit = FastXRFLinearFit()
+        fastFit.setFitConfigurationFile(self.config_file)
+
+        outbuffer = OutputBuffer(outputDir=self.out_dir_elements,
+                        outputRoot= elem_file_name,
+                        fileEntry= self.sample_name + '_%06u' % scan_nr,
+                        diagnostics=0,
+                        tif=make_elements_tif, edf=0, csv=1,
+                        h5=make_elements_h5, dat=0,
+                        multipage=0,
+                        overwrite=1)
+
+        with outbuffer.saveContext():
+            outbuffer = fastFit.fitMultipleSpectra(y=self.xrf_norm,
+                        weight=0,
+                        refit=1,
+                        concentrations=0,
+                        outbuffer=outbuffer)
+
+    # Fitting of average spectrum from image stack
+    def fitAvgSpectrumToFile(self):
+        spec_file = os.path.join(self.out_dir_spectrum + f'spectrum_{str(scan_nr).zfill(6)}_{self.channel}.h5')
+        print("Writing average-spectrum fit file: %s" % spec_file)
+        mca_fit = McaTheory()
+        conf = ConfigDict.ConfigDict()
+        conf.read(config_file)
+        mca_fit.setConfiguration(conf)
+        mca_fit.configure(conf)
+        mca_fit.setData(self.xrf_avg)
+        mca_fit.estimate()
+        fit_result = mca_fit.startfit(digest=0)
+        digest_file = os.path.join(self.out_dir_spectrum + f'spectrum_{str(scan_nr).zfill(6)}_{self.channel}.fit')
+        mca_fit_result = mca_fit.digestresult(outfile=digest_file)
+        #ConfigDict.prtdict(mca_fit_result)
+        with h5py.File(spec_file, 'w') as sfp:
+            self._recursive_save(sfp, 'mca_fit/', mca_fit_result)
+            sfp['measurement/I0_average'] = numpy.mean(self.I0)
+            sfp['measurement/dwell_time'] = self.dwell
+            sfp['measurement/energy'] = self.energy
+            sfp['measurement/detector'] = self.detector
+            sfp['measurement/channel'] = self.channel
+
+    # Internal function to save the dictionary with results of the avgerage fitting to an h5 file.
+    def _recursive_save(self, h5file, path, dic):
+        for key, item in dic.items():
+            key = str(key)
+            #print('key=' + path + key)
+            if isinstance(item, list):
+                item = numpy.array(item)
+            # save strings, numpy.int64, and numpy.float64 types
+            if isinstance(item, (numpy.int64, numpy.float64, str, numpy.float, float, numpy.float32,int)):
+                h5file[path + key] = item
+            # save numpy arrays
+            elif isinstance(item, numpy.ndarray):
+                try:
+                    h5file.create_dataset(path + key, data=item, compression='gzip')
+                except:
+                    #print(path + key, item)
+                    item = numpy.array(item).astype('|S9')
+                    h5file.create_dataset(path + key, data=item)
+            # save dictionaries
+            elif isinstance(item, dict):
+                self._recursive_save(h5file, path + key + '/', item)
+            # other types cannot be saved and will result in an error
+            #else:
+                #print('Cannot save key=%s'%(path+key))
+
+##############################################################################
+
+if __name__ == "__main__":
+
+    # read which scan to work on
+    scan_nr = int(sys.argv[1])
+    t0 = time.time()
+
+    # create an instance of the fit class for each channel
+    fits = {}
+    for channel in channels:
+        fits[str(channel)] = xrfBatch(session_path = session_path, 
+                                      sample_name  = sample_name, 
+                                      scan_nr      = scan_nr,
+                                      config_file  = config_file,
+                                      detector     = detector,
+                                      channel      = channel)
+
+    # read data  
+    for channel in channels:
+        fits[str(channel)].readData(I0_scale_factor)
+        print("Raw data file read. Time elapsed = %s seconds" % int(time.time() - t0))
+
+    # write  a pyMCA readable h5 file 
+    if(make_pymca_h5):
+        for channel in channels:
+            fits[str(channel)].writePymcafile()
+            print("PyMCA compatible file written. Time elapsed = %s seconds" % int(time.time() - t0))
+
+    # fit the elements according to a given config file
+    if(make_elements_h5 or make_elements_tif):
+        for channel in channels:
+            fits[str(channel)].fitElementsToFile(make_elements_h5, make_elements_tif)
+            print("Elemental maps file written. Time elapsed = %s seconds " % int(time.time() - t0))
+
+    # fit the average spectrum
+    if(make_spectrum_fit_h5):
+        for channel in channels:
+            fits[str(channel)].fitAvgSpectrumToFile()
+            print("Average spectrum fit file written. Time elapsed = %s seconds" % int(time.time() - t0))
